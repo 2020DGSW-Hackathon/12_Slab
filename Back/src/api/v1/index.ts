a0962667b6ed16292d8b94d1114f5e969d399f5b
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-10 16:50:20 +0900

    delete

--- Back/src/api/v1/index.ts
+++ Back/src/api/v1/index.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 import auth from './auth';
 import user from './user';
-import item from './item';
-import team from './team';
 import upload from './upload';
 import laboratory from './laboratory';
 import major from './major';
@@ -10,8 +8,6 @@ const router = Router();
 
 router.use('/auth', auth);
 router.use('/user', user);
-router.use('/item', item);
-router.use('/team', team);
 router.use('/upload', upload);
 router.use('/laboratory', laboratory);
 router.use('/major', major);
